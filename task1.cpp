7677e0ed3a6e07bc4db984c54b00d2ad1362ae4f
Soskiiit <NikitaA439@gmail.com>
2024-09-23 16:36:43 +0300

    1, 2, 3 tasks

--- task1.cpp
+++ task1.cpp
@@ -1,3 +1,30 @@
-//
-// Created by archi on 23.09.24.
-//
+#include <iostream>
+
+namespace {
+const int kStartOfSequence = 1;
+const int kMustDivide = 5;
+}
+
+long calculate(int n, int m) {
+    long sumOfSeries = 0;
+    for (int i = kStartOfSequence; i <= n; i++) {
+        if (i % m != 0 && i % kMustDivide == 0) {
+            sumOfSeries += i;
+        }
+    }
+    return sumOfSeries;
+}
+
+int main(int, char**) {
+    int n = 0;
+    int m = 0;
+    char isWorks = 'y';
+    while (isWorks == 'y') {
+        std::cout << "Введите числа n и m через пробел: ";
+        std::cin >> n >> m;
+        std::cout << calculate(n, m) << '\n';
+        std::cout << "Продолжаем работу? (y/n) ";
+        std::cin >> isWorks;
+    }
+    return 0;
+}
--- task2.cpp
+++ task2.cpp
@@ -1,3 +1,43 @@
-//
-// Created by archi on 23.09.24.
-//
+#include <cmath>
+#include <iostream>
+
+namespace {
+const int kStartSequenceNegative = 3;
+const int kEndSequenceNegative = 9;
+const int kStepSequenceNegative = 3;
+
+const int kStartSequenceNonNegative = 2;
+const int kEndSequenceNonNegative = 8;
+const int kStepSequenceNonNegative = 2;
+
+const int kMinNonNegativeNumber = 0;
+}
+
+double productIfNegative() {
+    double answer = 1;
+    for (int i = kStartSequenceNegative; i <= kEndSequenceNegative; i += kStepSequenceNegative) {
+        answer *= i - 2;
+    }
+    return answer;
+}
+
+double productIfNonNegative(double a) {
+    double answer = 1;
+    for (int i = kStartSequenceNonNegative; i <= kEndSequenceNonNegative; i += kStepSequenceNonNegative) {
+        answer *= i * i - a;
+    }
+    return answer;
+}
+
+int main(int, char**) {
+    double a;
+    std::cout << "Введите число а: ";
+    std::cin >> a;
+    std::cout << "Сумма ряда S равна: ";
+    if (a < kMinNonNegativeNumber) {
+        std::cout << productIfNegative() << std::endl;
+    } else {
+        std::cout << productIfNonNegative(a) << std::endl;
+    }
+    return 0;
+}
