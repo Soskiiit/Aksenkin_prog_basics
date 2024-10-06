7ee8e2551174aabbcc66bdde356a348d7aec4db1
Soskiiit <NikitaA439@gmail.com>
2024-10-06 15:52:27 +0300

    lab 3 v1.0.1 fix ci/cd

--- include/calculations.h
+++ include/calculations.h
@@ -1,10 +1,8 @@
 #pragma once
 
-namespace {
-    const double eps = 1e-12;
-}
-
 namespace calculations {
+    const double eps = 1e-12;
+
     double binaryCalculations(double precision=eps);
     double newtonsMethod (double precision=eps);
     double iterativeMethod(double delta=eps);
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,13 @@
 
 #include "calculations.h"
 
+namespace {
+const int precision = 12;
+}
+
 int main() {
-    std::cout << "Результат с помощью бинарного поиска: " << "\t" << std::setprecision(12) << calculations::binaryCalculations() << std::endl;
-    std::cout << "Результат с помощью метода Ньютона: " << "\t" << std::setprecision(12) << calculations::newtonsMethod() << std::endl;
-    std::cout << "Результат с помощью метода итераций: " << "\t" << std::setprecision(12) << calculations::iterativeMethod() << std::endl;
+    std::cout << "Результат с помощью бинарного поиска: " << "\t" << std::setprecision(precision) << calculations::binaryCalculations() << std::endl;
+    std::cout << "Результат с помощью метода Ньютона: " << "\t" << std::setprecision(precision) << calculations::newtonsMethod() << std::endl;
+    std::cout << "Результат с помощью метода итераций: " << "\t" << std::setprecision(precision) << calculations::iterativeMethod() << std::endl;
     return 0;
 }
